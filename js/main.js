2c7f15a6fad337707edbe7e196c7a818382892f2
Cristina-F-R <cristina.faixedas@gmail.com>
2023-02-03 11:31:28 +0100

    03/02/23

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const kittenRaceOne = '<h4 class="card_race">Siamés</h4>';
 
 const kittenImageTwo = '<img class="card_img"src="https://dev.adalab.es/sphynx-gato.webp"      alt="sphynx-cat"/>';
 const kittenNameTwo = '<h3 class="card_title">Fiona</h3>';
-const kittenDescriptionTwo = '<p class="card_description">Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo. </p>';
+const kittenDescriptionTwo = '<p class="card_description"> elegante Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo. </p>';
 const kittenRaceTwo = '<h4 class="card_race">Sphynx</h4>';
 
 const kittenImageThree = '<img class="card_img" src="https://dev.adalab.es/maine-coon-cat.webp"   alt="maine-coon-cat" />';
@@ -33,5 +33,40 @@ const kittenOne = `<li>${kittenImageOne} ${kittenNameOne.toUpperCase()} ${kitten
 const kittenTwo = `<li>${kittenImageTwo} ${kittenNameTwo.toUpperCase()} ${kittenRaceTwo} ${kittenDescriptionTwo}</li>`;
 const kittenThree = `<li>${kittenImageThree} ${kittenNameThree.toUpperCase()} ${kittenRaceThree} ${kittenDescriptionThree}</li>`;
 
-kitten.innerHTML=kittenOne + kittenTwo + kittenThree;
+// kitten.innerHTML=kittenOne + kittenTwo + kittenThree;
 
+
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
+
+
+
+if( kittenDescriptionOne.includes(descrSearchText) ) {
+    kitten.innerHTML = kittenOne;
+}
+
+if( kittenDescriptionTwo.includes(descrSearchText) ) {
+    kitten.innerHTML += kittenTwo;
+}
+
+if( kittenDescriptionThree.includes(descrSearchText) ) {
+    kitten.innerHTML += kittenThree;
+}
+
+
+
+const race = document.querySelectorAll('.card_race');
+let kittenRace = race.value;
+
+let html = '';
+
+
+
+if (kittenRace === "") {
+  html = `Uy que despiste, no sabemos su raza`;
+  console.log(html);
+} else {
+  html = kittenRace;
+  console.log(html);
+}
